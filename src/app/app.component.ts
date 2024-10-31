17b98386a1351c21b394954a25b30dbe311aa910
Horvath Mark <brryy0371@gmail.com>
2024-10-31 11:50:15 +0100

    modelek létrehozása

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChallangeListComponent } from "./challange-list/challange-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChallangeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/challange-details/challange-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challange-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './challange-details.component.html',
+  styleUrl: './challange-details.component.css'
+})
+export class ChallangeDetailsComponent {
+
+}
